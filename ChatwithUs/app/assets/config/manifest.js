bfd930606ec34a169990faa80d2904a2be331daa
minakshi2330 <minut9056@gmail.com>
2024-06-20 17:37:23 +0530

    Chat App

---
+++ ChatwithUs/app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link jquery.min.js
+//= link popper.min.js
+//= link bootstrap.min.js
+//= link semantic.min.js
---
+++ ChatwithUs/app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
